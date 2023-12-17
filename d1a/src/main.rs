d1bc2d7dbaeb16445ef8854a193c5abdf6a00d39
thomas hryckewycz <24668401+tavichh@users.noreply.github.com>
2023-12-17 04:35:46 -0500

    learned a new word

--- d1a/src/main.rs
+++ d1a/src/main.rs
@@ -1,24 +1,30 @@
+use std::fs::File;
+use std::vec;
 use std::{
-    f32::RADIX,
-    fs::{self, File},
     io::{BufRead, BufReader},
     path::Path,
 };
 
 fn main() {
     let lines = get_line_buffer("input.txt");
+    let mut sums: Vec<u32> = vec![];
     for line_index in 0..lines.len() {
-        let mut line_value: Vec<u32> = vec![];
+        let mut line_value: Vec<char> = vec![];
         let letters: Vec<char> = lines[line_index].chars().collect();
         for letter_index in 0..letters.len() {
             if letters[letter_index].is_numeric() {
-                line_value.push(letters[letter_index].to_digit(RADIX).unwrap());
+                line_value.push(letters[letter_index]);
+            }
+            for number in &line_value {
+                sums.push(char::to_digit(*number, 10).unwrap());
             }
         }
+        let sum: u32 = sums.iter().sum();
+        println!("{}", sum);
     }
 }
 
-fn get_line_buffer(file: impl AsRef<Path>) -> Vec<String> {
+fn get_line_buffer(_file: impl AsRef<Path>) -> Vec<String> {
     let input = File::open("input.txt").expect("Failed to open file");
     let line_buffer = BufReader::new(input);
     line_buffer
